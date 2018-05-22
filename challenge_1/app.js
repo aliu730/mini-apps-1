6ce849aa0d7b44618da737064041c8d6bc6ead7b
aliu730 <aliu730@gmail.com>
2018-05-21 19:55:39 -0700

    columns?

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,25 +1,25 @@
 //place piece function drops X or O.
-let piece = 0;
-let x_wins = 0;
-let o_wins = 0;
-let games = 0;
-let realBoard = [[],[],[]];
+var piece = 0;
+var x_wins = 0;
+var o_wins = 0;
+var games = 0;
+var realBoard = [[],[],[]];
 //generate a board
-let boardMaker = () => {
+var boardMaker = () => {
   piece = 0;
   games++;
-  let lineBreak = document.createElement('div');
+  var lineBreak = document.createElement('div');
   lineBreak.innerHTML = ("GAME "+ games);
   document.getElementById('board').appendChild(lineBreak);
-  let r0 = document.createElement('div');
-  let r1 = document.createElement('div');
-  let r2 = document.createElement('div');
+  var r0 = document.createElement('div');
+  var r1 = document.createElement('div');
+  var r2 = document.createElement('div');
   r0.id ="r0";
   r1.id ="r1"; 
   r2.id ="r2";
   
-  for (let i = 0; i < 3; i++) {
-    let cell = document.createElement('span');
+  for (var i = 0; i < 3; i++) {
+    var cell = document.createElement('span');
     cell.id ="r0-" + i;
     cell.onclick = function() {
       placePiece(this);
@@ -28,7 +28,7 @@ let boardMaker = () => {
     cell.style ="border: 1px solid black; width: fit-content;";
     r0.appendChild(cell);
     //now make r1
-    let cell1 = document.createElement('span');
+    var cell1 = document.createElement('span');
     cell1.id ="r1-" + i;
     cell1.innerHTML = " _ ";
     cell1.onclick = function() {
@@ -37,7 +37,7 @@ let boardMaker = () => {
     cell1.style = "border: 1px solid black; width: fit-content;";
     r1.appendChild(cell1);
     //r2
-    let cell2 = document.createElement('span');
+    var cell2 = document.createElement('span');
     cell2.id = "r2-" + i;
     cell2.innerHTML = " _ ";
     cell2.onclick = function() {
@@ -52,9 +52,9 @@ let boardMaker = () => {
   document.getElementById('board').appendChild(r2);
 };
 boardMaker();
-let placePiece = (cell) => {
-  let row = (cell.id[1]);
-  let column = (cell.id[3]);
+var placePiece = (cell) => {
+  var row = (cell.id[1]);
+  var column = (cell.id[3]);
   if (piece % 2 === 0 && cell.innerHTML !== "X" && cell.innerHTML !== "O") {
     cell.innerHTML = 'X';
     realBoard[row][column] = 'X';
@@ -70,21 +70,75 @@ let placePiece = (cell) => {
   checkDiag1();
   checkDiag2();
   checkTie();
-  //console.log(realBoard);
+  checkColumns();
 };
-
-
-// console.log("connected");
-//check Win func
-let checkWinR0 = () => {
-  let count_x = 0;
-  let count_o = 0;
-  // might just be hard coding.
-  // intially wanted to tie this to a nested array.
-  
-  // check row 0 win?
-  for (let i = 0; i < 3; i++) {
-    //console.log(document.getElementById('r0-'+i));
+var checkColumns = () => {
+  var col0x = 0;
+  var col0o = 0;
+  var col1x = 0;
+  var col1o = 0;
+  var col2x = 0;
+  var col2o = 0;
+  for (var i = 0; i < 3; i++) {
+    if (realBoard[i][0] === "X") {
+      col0x++;
+      if (col0x === 3) {
+        alert("PLAYER X WON");
+        x_wins++;
+        document.getElementById('xwon').innerHTML = x_wins;
+      }
+    }
+    if (realBoard[i][0] === "O") {
+      col0o++;
+      if (col0o === 3) {
+        alert("PLAYER O WON");
+        o_wins++;
+        document.getElementById('owon').innerHTML = o_wins;
+        resetBoard();
+      }
+    }
+    if (realBoard[i][1] === "X") {
+      col1x++;
+      if (col1x === 3) {
+        alert("PLAYER X WON");
+        x_wins++;
+        document.getElementById('xwon').innerHTML = x_wins;
+        resetBoard();
+      }
+    }
+    if (realBoard[i][1] === "O") {
+      col1o++;
+      if (col1o === 3) {
+        alert("PLAYER O WON");
+        o_wins++;
+        document.getElementById('owon').innerHTML = o_wins;
+        resetBoard();
+      }
+    }
+    if (realBoard[i][2] === "X") {
+      col2x++;
+      if (col2x === 3) {
+        alert("PLAYER X WON");
+        x_wins++;
+        document.getElementById('xwon').innerHTML = x_wins;
+        resetBoard();
+      }
+    }
+    if (realBoard[i][2] === "O") {
+      col2o++;
+      if(col2o === 3) {
+        alert("PLAYER O WON");
+        o_wins++;
+        document.getElementById('owon').innerHTML = o_wins;
+        resetBoard();
+      }
+    }
+  }
+}
+var checkWinR0 = () => {
+  var count_x = 0;
+  var count_o = 0;
+  for (var i = 0; i < 3; i++) {
     if (realBoard[0][i] === 'X') {
       count_x++;
       if(count_x === 3) {
@@ -106,14 +160,11 @@ let checkWinR0 = () => {
       }
     }
   }
-  // check row 1 win?
-  // check row 2 win?
 };
-let checkWinR1 = () => {
-  let count_x = 0;
-  let count_o = 0;
-  for (let i = 0; i < 3; i++) {
-    //console.log(document.getElementById('r0-'+i));
+var checkWinR1 = () => {
+  var count_x = 0;
+  var count_o = 0;
+  for (var i = 0; i < 3; i++) {
     if (realBoard[1][i] === 'X') {
       count_x++;
       if(count_x === 3) {
@@ -136,11 +187,10 @@ let checkWinR1 = () => {
     }
   }
 }
-let checkWinR2 = () => {
-  let count_x = 0;
-  let count_o = 0;
-  for (let i = 0; i < 3; i++) {
-    //console.log(document.getElementById('r0-'+i));
+var checkWinR2 = () => {
+  var count_x = 0;
+  var count_o = 0;
+  for (var i = 0; i < 3; i++) {
     if (realBoard[2][i] === 'X') {
       count_x++;
       if(count_x === 3) {
@@ -163,10 +213,10 @@ let checkWinR2 = () => {
     }
   }
 }
-let checkDiag1 = () => {
-  let count_x = 0;
-  let count_o = 0;
-  for (let i = 0; i < 3; i++) {
+var checkDiag1 = () => {
+  var count_x = 0;
+  var count_o = 0;
+  for (var i = 0; i < 3; i++) {
     if (realBoard[i][i] === "X") {
       count_x++;
       if (count_x === 3) {
@@ -187,10 +237,10 @@ let checkDiag1 = () => {
     }
   }
 };
-let checkDiag2 = () => {
-  let count_x = 0;
-  let count_o = 0;
-  for (let i = 0; i < 3; i++) {
+var checkDiag2 = () => {
+  var count_x = 0;
+  var count_o = 0;
+  for (var i = 0; i < 3; i++) {
     if (realBoard[i][2-i] === "X") {
       count_x++;
       if (count_x === 3) {
@@ -211,7 +261,7 @@ let checkDiag2 = () => {
     }
   }
 };
-let checkTie = () => {
+var checkTie = () => {
   var counter = 0;
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
@@ -225,12 +275,10 @@ let checkTie = () => {
     resetBoard();
   }
 };
-//console.log(document.getElementsByClassName('xtitle').innerHTML)
-let resetBoard = () => {
-  //remove all rows.
-  let r0 = document.getElementById('r0');
-  let r1 = document.getElementById('r1');
-  let r2 = document.getElementById('r2');
+var resetBoard = () => {
+  var r0 = document.getElementById('r0');
+  var r1 = document.getElementById('r1');
+  var r2 = document.getElementById('r2');
   r0.parentNode.removeChild(r0);
   r1.parentNode.removeChild(r1);
   r2.parentNode.removeChild(r2);

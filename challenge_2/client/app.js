41e60762575a455cbc6f77f37b4a170b31ae8266
aliu730 <aliu730@gmail.com>
2018-05-22 15:27:36 -0700

    working on handling post req

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,1 +1,21 @@
-// renders the html?
+$(document).ready(function () {
+  $('form').submit(function(event) { // on submit click
+    //console.log($("input:first").val());
+    //make our ajax pass to our express server
+    var text = ($('input:first').val());
+    // console.log(text);
+    $.ajax({
+      type: 'POST',
+      url: "http://localhost:3000/submission",
+      datatype: 'application/json',
+      data: JSON.stringify(text),
+      success: function(data) {
+        console.log(data);
+      },
+      error: function() {
+        console.log("err");
+      }
+    });
+    event.preventDefault();
+  });
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,11 +2,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
-  res.send("testing");
+app.post('/submission', function(req,res) {
+  console.log(req.body); 
 });
 
+// app.get('/allPreviousFiles', function(req,res) {
+//   res.status(200).end('Hello');
+// });
+
+app.use(express.static('client'));
 //TODO: app.post
 app.listen(3000, function() {
   console.log("listening to port 3000");
-})
+});
